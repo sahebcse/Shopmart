e8aa6162f79d54dc8d555a844acba1edd3ae3c7b
Bibhash-Kumar <bibhashbest12@gmail.com>
2021-11-07 16:41:11 +0530

    Basic product page

--- client/src/component/Views/Navbar.js
+++ client/src/component/Views/Navbar.js
@@ -2,32 +2,32 @@ import React, { useEffect, useState } from "react";
 
 import { Grid, Button, Typography } from "@material-ui/core";
 import CreateRoomIcon from "@material-ui/icons/RoomOutlined";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
-import {NotificationManager} from 'react-notifications'
+import { Link } from "react-router-dom";
+import { NotificationManager } from "react-notifications";
 
-import logo from "./logo.jpg"
+import logo from "./logo.jpg";
 
 const Navbar = (props) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = JSON.parse(localStorage.getItem('profile'))
-  
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   const handleLogin = () => {
-    navigate('/signIn');
+    navigate("/signIn");
   };
 
   const handleLogout = () => {
-    dispatch({type:"LOGOUT"});
-    NotificationManager.error("logging Out")
-    navigate('/');
+    dispatch({ type: "LOGOUT" });
+    NotificationManager.error("logging Out");
+    navigate("/");
     // history.go(0);
   };
 
-  const hanldeOpenAllStreams = ()=>{
-    navigate('/streams');
-  }
+  const hanldeOpenAllStreams = () => {
+    navigate("/streams");
+  };
 
   const [navbarOpen, setNavbarOpen] = React.useState(false);
 
@@ -41,14 +41,8 @@ const Navbar = (props) => {
       }
     >
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-      
-
         <div className="w-full relative flex justify-between lg:w-auto lg:static   lg:justify-start">
-          
-           
-          <img src={logo} className="w-10 h-10 mr-2"/>
-
-           
+          <img src={logo} className="w-10 h-10 mr-2" />
 
           <Link
             className={
@@ -81,23 +75,41 @@ const Navbar = (props) => {
         >
           <ul className="flex flex-col lg:flex-row list-none lg:ml-auto mr-8 text-lg">
             <li className="flex items-center">
-            <button className="px-2 py-1 mr-2 bg-blue-300 text-indigo-800 hover:bg-blue-500 hover:text-white rounded" onClick={()=>window.open(' ')}>Orders</button>
-              {
-                user?(<button className="px-2 py-1 mr-2 bg-green-300 text-indigo-800 hover:bg-green-500 hover:text-white rounded" onClick={()=>navigate('/user/dashboard')}>Dashboard</button>):<span></span>
-              }
-              {user && <button className="px-2 py-1 mr-2 bg-yellow-300 text-indigo-800 hover:bg-yellow-500 hover:text-white rounded" onClick={hanldeOpenAllStreams}>Stores</button>}
+              <button
+                className="px-2 py-1 mr-2 bg-blue-300 text-indigo-800 hover:bg-blue-500 hover:text-white rounded"
+                onClick={() => window.open(" ")}
+              >
+                Orders
+              </button>
+              {user ? (
+                <button
+                  className="px-2 py-1 mr-2 bg-green-300 text-indigo-800 hover:bg-green-500 hover:text-white rounded"
+                  onClick={() => navigate("/user/dashboard")}
+                >
+                  Dashboard
+                </button>
+              ) : (
+                <span></span>
+              )}
+              {user && (
+                <button
+                  className="px-2 py-1 mr-2 bg-yellow-300 text-indigo-800 hover:bg-yellow-500 hover:text-white rounded"
+                  onClick={hanldeOpenAllStreams}
+                >
+                  Stores
+                </button>
+              )}
               <Grid item xs={12} md={4}>
-                {user ? (<span>
-                  
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    onClick={handleLogout}
-                  >
-                    Logout
-                  </Button>
+                {user ? (
+                  <span>
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      onClick={handleLogout}
+                    >
+                      Logout
+                    </Button>
                   </span>
-                  
                 ) : (
                   <Button
                     variant="outlined"
@@ -124,8 +136,3 @@ const Navbar = (props) => {
 };
 
 export default Navbar;
-
-
-
-
- 
