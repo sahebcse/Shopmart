381c9a612e91472142eb0f74082c6a2f948314d6
ishanmadhav <74528682+ishanmadhav@users.noreply.github.com>
2021-11-07 12:51:38 +0530

    Backend Refactor

---
+++ server/app.js
@@ -0,0 +1,43 @@
+//Library Imports
+const cors=require('cors')
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+
+//Connection Data
+const PORT=process.env.PORT||5000
+//const CONNECTION_URL='mongodb://localhost:27017/daintree'
+const CONNECTION_URL='mongodb+srv://jacksapera:lsY8V3rFbSUfpr3Z@cluster0.qadkz.mongodb.net/Daintree?retryWrites=true&w=majority'
+
+//DB Connection
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+//Utilites
+app.use(cors())
+app.use(express.json({limit:"30mb", extended:true}))
+app.use(express.urlencoded({limit:"30mb", extended:true}))
+
+//Routes
+const userRoutes=require('./routes/userRoutes')
+const merchantRoutes=require('./routes/merchantRoutes')
+const productRoutes=require('./routes/productRoutes')
+const orderRoutes=require('./routes/orderRoutes')
+
+
+app.use('/', userRoutes)
+app.use('/', merchantRoutes)
+app.use('/', productRoutes)
+app.use('/', orderRoutes)
+
+
+
+
+app.get('/',(req, res)=>
+{
+    res.send('Hey')
+})
+
+app.listen(PORT, ()=>
+{
+    console.log("Daintree server up and running")
+})
